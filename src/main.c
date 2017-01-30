d96f946b06047af3560afdf432da4c0614542c56
Mathieu Sauvau <mathieu.sauvau@epitech.eu>
2017-01-30 19:45:27 +0100

    change main test

--- src/main.c
+++ src/main.c
@@ -5,17 +5,24 @@
 int main()
 {
   char c[1];
-  void *ptr = malloc(105);
-  show_alloc_mem();
-  free(ptr);
+  /* void *ptr = malloc(105); */
+  /* show_alloc_mem(); */
+  /* free(ptr); */
   //free(c);
-  ptr = malloc(4097);
-  show_alloc_mem();
+  //  ptr = malloc(4097);
+  //  show_alloc_mem();
   //free(ptr);
   //ptr = malloc(4097);
-  void *ptr1 = malloc(180);
-  void *ptr2 = malloc(105);
-  void *ptr3 = malloc(110);
-  void *ptr4 = malloc(120);
+  void *ptr1 = malloc(1056);
+  void *ptr2 = malloc(1000);
+  // void *ptr3 = malloc(1000);
+  //  void *ptr4 = malloc(500);
+  show_alloc_mem();
+
+  free(ptr1);
+  free(ptr2);
+  //  free(ptr3);
+  //  free(ptr4);
+  //ptr1 = malloc(1000);
   show_alloc_mem();
 }
